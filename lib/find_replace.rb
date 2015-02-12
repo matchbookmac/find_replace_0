ee0a96b7214a0d2d74d96739a21eed0db3a8df88
Ian <iMacD2.0@gmail.com>
2015-02-11 23:45:58 -0800

    find_replace ignores character case. Website works.

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -3,9 +3,11 @@ require('pry')
 class String
   define_method(:find_replace) do |find, replace_with|
     text = self.split()
+    find_downcase = find.downcase()
     change = false
     text.each().with_index() do |word, x|
-      if word.eql?(find)
+      current_word = word.downcase()
+      if current_word.eql?(find_downcase)
         text[x] = replace_with
         change = true
       end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -3,9 +3,9 @@ require('find_replace')
 
 describe('String#find_replace') do
   it('Searches for the argument word in the object string, replaces the word if found') do
-    expect('hello world'.find_replace('hello', 'goodbye')).to(eq('goodbye world'))
+    expect('Hello World'.find_replace('hello', 'goodbye')).to(eq('goodbye World'))
   end
   it('Searches for the argument word in the object string, returns original string if not found') do
-    expect('hello world'.find_replace('huh', 'goodbye')).to(eq('hello world'))
+    expect('Hello world'.find_replace('huh', 'goodbye')).to(eq('Hello world'))
   end
 end
