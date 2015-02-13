5e0e54faf3ed7d56878da53cb0f4a9d5bbcec1cc
Ian <iMacD2.0@gmail.com>
2015-02-13 00:26:43 -0800

    Working find_replace method for partial matches

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -4,9 +4,9 @@ class String
   define_method(:find_replace) do |find, replace_with|
     text = self.split()
     change = false
-    text.each().with_index() do |word, x|
-      if word.downcase().eql?(find.downcase())
-        text[x] = replace_with
+    text.each().with_index() do |word, text_index|
+      if word.downcase().scan(find.downcase()).join().eql?(find.downcase())        
+        text[text_index] = word.downcase().gsub(find.downcase(), replace_with)
         change = true
       end
     end
@@ -16,7 +16,5 @@ class String
     elsif change.==(false)
       self
     end
-  end 
+  end
 end
-
-# try String#scan
