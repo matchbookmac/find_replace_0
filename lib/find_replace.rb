79ed324931383a8697874ec0af22c13e9ffc072c
Ian <iMacD2.0@gmail.com>
2015-02-12 18:38:58 -0800

    Refactor of find_replace method

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -3,11 +3,9 @@ require('pry')
 class String
   define_method(:find_replace) do |find, replace_with|
     text = self.split()
-    find_downcase = find.downcase()
     change = false
     text.each().with_index() do |word, x|
-      current_word = word.downcase()
-      if current_word.eql?(find_downcase)
+      if word.downcase().eql?(find.downcase())
         text[x] = replace_with
         change = true
       end
